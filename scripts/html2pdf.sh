6629c6da1323941828228f0effcf41eef2942116
opoto <olivier.potonniee@gmail.com>
2016-09-30 10:04:15 +0200

    Display file paths

--- scripts/html2pdf.sh
+++ scripts/html2pdf.sh
@@ -12,13 +12,14 @@ then
   exit 1
 fi
 
-echo "Running..."
-
 #PROXY=--proxy $http_proxy
 #INPUT=http://globalplatform.github.io/WebApis-for-SE/doc/
 INPUT=doc/index.html
 OUTPUT=build/WebAPI-for-SE-v1.0.pdf
 
+echo "INPUT: $INPUT"
+echo "OUTPUT: $OUTPUT"
+echo "Processing..."
+
 mkdir -p build
 wkhtmltopdf $PROXY --header-left 'Web API For Accessing Secure Element - v1.0' --header-right '[page]/[toPage]' --header-line --margin-top 2.3cm --header-spacing 8 --header-font-size 10 --footer-line --footer-font-name 'Times New Roman' --footer-font-size 10 --footer-spacing 8 --margin-bottom 2.3cm  --footer-center 'Copyright © 2016 GlobalPlatform Inc. All Rights Reserved.' --debug-javascript  $INPUT $OUTPUT
-
